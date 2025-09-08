28c83bbf30f6793cdbf2e6d83caa066971c80278
nakazonoyuta <nakazono.yuta@mikilab.doshisha.ac.jp>
2025-09-08 22:29:28 +0900

    feat(backend)：作成したビューをどのURLで呼び出すかを設定

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from ninja import NinjaAPI
@@ -43,6 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
     path('health/', health),
+    path('api/posts/', include('posts.urls')), 
 ]
 
 if settings.DEBUG:
