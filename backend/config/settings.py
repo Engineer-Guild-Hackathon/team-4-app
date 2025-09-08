eb03bf740234fcf273ab4aed76b5cb7f5e8360bb
nakazonoyuta <nakazono.yuta@mikilab.doshisha.ac.jp>
2025-09-08 21:48:24 +0900

    feat(frontend)：作成したアプリをDjangoに認識させ，メディアファイルを保存するための設定を追加

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'topics',
     'ninja_extra',
     'ninja_jwt',
+    'posts'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media settings
+# MEDIA_ROOT: アップロードされたファイルが実際に保存される場所
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# MEDIA_URL: ブラウザからファイルにアクセスするためのURL
+MEDIA_URL = '/media/'
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from ninja import NinjaAPI
 from ninja_jwt.routers.obtain import obtain_pair_router
 from ninja_jwt.authentication import JWTAuth
@@ -42,3 +44,6 @@ urlpatterns = [
     path('api/', api.urls),
     path('health/', health),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
