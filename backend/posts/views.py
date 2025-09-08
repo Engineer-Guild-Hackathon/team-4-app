13967c1c62b53ab754ba0fc480f5b8060407a3fa
nakazonoyuta <nakazono.yuta@mikilab.doshisha.ac.jp>
2025-09-08 22:02:28 +0900

    feat(backend)：ビューの作成

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostCreateAPIView(generics.CreateAPIView):
+    """投稿を作成するためのAPIビュー"""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    # ファイルアップロードを扱うためにパーサーを設定
+    parser_classes = (MultiPartParser, FormParser)
