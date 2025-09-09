ba6c22d7d559a5a358c2c6a5081f5d9f3a3f723f
kotajokei <kotajokei@gmail.com>
2025-09-09 15:00:58 +0900

    hot-fix(backend): Topicアドミンのカラム表示設定が不適切だったため修正

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -3,8 +3,8 @@ from .models import Post
 
 @admin.register(Post)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'updated_at')
+    list_display = ('content', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
-    search_fields = ('title', 'description')
+    search_fields = ('content',)
     readonly_fields = ('id', 'created_at', 'updated_at')
     ordering = ('-created_at',)
