1226def079203720e88186b08b7efd94a3373ea3
c-hikita <chloecoding14@gmail.com>
2025-09-10 00:39:03 +0900

    feat(backend): 木構造モデルの構築

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,6 +134,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+AUTH_USER_MODEL = 'users.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    rank = models.IntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        verbose_name='ランク',
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.username
