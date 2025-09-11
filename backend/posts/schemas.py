c7b681970b1107a56783657cd19de292ab5349aa
nakazonoyuta <nakazono.yuta@mikilab.doshisha.ac.jp>
2025-09-11 14:06:23 +0900

    fix(backend)：スキーマの修正

--- backend/posts/schemas.py
+++ backend/posts/schemas.py
@@ -1,6 +1,7 @@
 from ninja import Schema
 from datetime import datetime
 from typing import List
+from .models import Post
 
 # レスポンスでメディア情報を返すためのスキーマ
 class PostMediaOut(Schema):
@@ -12,4 +13,10 @@ class PostOut(Schema):
     id: int
     content: str
     created_at: datetime
-    media: List[PostMediaOut] # メディアをリストとして含める
+    media: List[PostMediaOut]
+
+    # 'media'フィールドを解決するためのリゾルバ
+    @staticmethod
+    def resolve_media(obj: Post) -> List[PostMediaOut]:
+        # postオブジェクト(obj)に紐づく全てのmediaをリストにして返す
+        return list(obj.media.all())
