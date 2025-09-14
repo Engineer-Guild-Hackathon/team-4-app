79a3b505ecbfe1cc55fcb8529e2594ccdcf4b143
nakazonoyuta <nakazono.yuta@mikilab.doshisha.ac.jp>
2025-09-15 02:43:19 +0900

    fix(backend): インポートの変更

--- backend/config/storages.py
+++ backend/config/storages.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from storages.backends.s3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
+
 
 class Storage(S3Boto3Storage):
     bucket_name = settings.CLOUDFLARE_R2_BUCKET_NAME
